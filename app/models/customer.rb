ac27cdddfffb5e45cee82aa559720fc613142e9b
John Setiawan <jsetiawan@capterra.com>
2015-10-05 17:00:20 -0400

    Forgot to add message.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :email, :phone
-  validates_format_of :website, with: /www\..+\..+/
+  validates_format_of :website, with: /www\..+\..+/, message: 'invalid url'
 end
