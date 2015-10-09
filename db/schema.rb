346e6d1d45ebcdfedc71b19659763e8419321478
John Setiawan <jsetiawan@capterra.com>
2015-10-09 14:42:35 -0400

    Add credit card association in db.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009174008) do
+ActiveRecord::Schema.define(version: 20151009175507) do
 
   create_table "credit_cards", force: :cascade do |t|
     t.integer  "number"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20151009174008) do
     t.string   "cardholder_name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "customer_id"
   end
+
+  add_index "credit_cards", ["customer_id"], name: "index_credit_cards_on_customer_id"
 
   create_table "customers", force: :cascade do |t|
     t.string   "first_name"
