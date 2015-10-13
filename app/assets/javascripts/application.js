24c30687b2ef8c7fb549b6f3d59ae3f771e7f0f8
John Setiawan <jsetiawan@capterra.com>
2015-10-13 16:48:10 -0400

    Add add another credit card button.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,25 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function () {
+    $('.js-add_credit_card').on('click', function () {
+        event.preventDefault();
+        var form = $('.js-credit_card_form').clone().removeClass('js-credit_card_form');
+        var unique_id = Date.now();
+
+        $('label', form).each(function (index) {
+            var for_text = $(this).attr('for');
+            $(this).attr('for', for_text.replace('0', unique_id));
+        });
+
+        $('input', form).each(function (index) {
+            var id_text = $(this).attr('id');
+            var name_text = $(this).attr('name');
+            $(this).attr('id', id_text.replace('0', unique_id));
+            $(this).attr('name', name_text.replace('0', unique_id));
+        });
+
+        $('.js-credit_card_form').after(form);
+    });
+});
